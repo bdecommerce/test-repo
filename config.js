58bfa84a86bf19d7729d81f35adda6c30c993e16
cheemakurthyteja <104550301+cheemakurthyteja@users.noreply.github.com>
2026-04-08 13:24:42 +0530

    adding few more files to repo to test

---
+++ config.js
@@ -0,0 +1,13 @@
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+var ccLauncherConfig = {
+    studentId: prompt("Please enter your Email address"),
+    studentName: prompt("Please enter your Full Name"),
+    postStudentData: false,
+    postUrl: ''
+};
